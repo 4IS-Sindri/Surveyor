f00b5a17fba0a84a4ed171cbd47d3230a4483bc1
4lch4 <git@4lch4.email>
2022-07-07 23:54:37 -0500

    feat: added available licenses to constants

---
+++ src/lib/constants/Licenses.ts
@@ -0,0 +1,11 @@
+export const AvailableLicenses = [
+  'GPL-3.0-or-later',
+  'GPL-3.0-only',
+  'LGPL-3.0-or-later',
+  'LGPL-3.0-only',
+  'MPL-2.0',
+  'Apache-2.0',
+  'MIT',
+  'BSL-1.0',
+  'Unlicense'
+]
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,2 +1,3 @@
 export * from './Author.js'
 export * from './Defaults.js'
+export * from './Licenses.js'
